a33d11a19257171c3312303af53deac6b3d8087d
Ivan Kolmychek <ivan.kolmychek@gmail.com>
2017-01-27 17:08:41 +0200

    Remove mouse wheel support from taglist buttons

--- rc.lua
+++ rc.lua
@@ -130,9 +130,7 @@ local taglist_buttons = awful.util.table.join(
                                               if client.focus then
                                                   client.focus:toggle_tag(t)
                                               end
-                                          end),
-                    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
-                    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+                                          end)
                 )
 
 local tasklist_buttons = awful.util.table.join(
